677e7aea4d396dc15e71defbbf4faf38b66b59d1
caichun0116 <1543943662@qq.com>
2024-12-04 14:46:46 +0800

    新增项目

--- testcases/test_sendcode.py
+++ testcases/test_sendcode.py
@@ -1,13 +1,9 @@
 # -*- coding: gbk -*-
-import requests
-from six import get_function_code
 
-from aip.query_code import query_code
+
 from data_assembly.data_request import get_request
 from data_assembly.data_url import get_url
 from utils.Request_method import get_tmethod
-from utils.log_utils import logger
-from utils.read_data import TotalPath
 
 
 # 组合URL以请求运动首页的热门游戏数据
